efa524397b6912de461698ade76f52215a44b91a
Amador Cuenca <sphi02ac@gmail.com>
2018-09-12 16:13:35 -0500

    feat: Import libraries and DB helper function

--- views.py
+++ views.py
@@ -1,11 +1,14 @@
-from flask import Flask
+import sqlite3
+from functools import wraps
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+app.config.from_object("_config")
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# helper functions
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE_PATH'])
 
 
 if __name__ == '__main__':
