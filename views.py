4661218edec532e5676bce4d6183761ed3373e3d
Amador Cuenca <sphi02ac@gmail.com>
2018-09-12 16:15:41 -0500

    feat: Add login decorator function

--- views.py
+++ views.py
@@ -11,5 +11,17 @@ def connect_db():
     return sqlite3.connect(app.config['DATABASE_PATH'])
 
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash("You need to login first.")
+            return redirect(url_for('login'))
+
+    return wrap
+
+
 if __name__ == '__main__':
     app.run()
