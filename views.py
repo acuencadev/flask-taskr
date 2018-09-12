3c4a047e77bb2ac72dd4a46d0993755cccfaa127
Amador Cuenca <sphi02ac@gmail.com>
2018-09-12 16:20:14 -0500

    feat: Add login function

--- views.py
+++ views.py
@@ -32,5 +32,19 @@ def logout():
     return redirect(url_for('login'))
 
 
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = "Invalid credentials. Please try again."
+            return render_template('login.html', error=error)
+        else:
+            session['logged_in'] = True
+            flash("Welcome!")
+            return redirect(url_for('tasks'))
+
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run()
