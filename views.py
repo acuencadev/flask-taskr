d80bbdf70be904a214a92438e03a7e1ea4d16c98
Amador Cuenca <sphi02ac@gmail.com>
2018-09-18 09:19:47 -0500

    feat: Add tasks route

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 import sqlite3
 from functools import wraps
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session, url_for, g
 
 app = Flask(__name__)
 app.config.from_object("_config")
@@ -44,6 +44,30 @@ def login():
             return redirect(url_for('tasks'))
 
     return render_template('login.html')
+
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+    g.db = connect_db()
+    cursor = g.db.execute("SELECT name, due_date, priority, task_id FROM tasks WHERE status = 1")
+    open_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cursor.fetchall()
+    ]
+
+    cursor = g.db.execute("SELECT name, due_date, priority, task_id FROM tasks WHERE status = 0")
+    closed_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cursor.fetchall()
+    ]
+
+    g.db.close()
+
+    return render_template(
+        'tasks.html',
+        form=AddTaskForm(request.form),
+        open_tasks=open_tasks,
+        closed_tasks=closed_tasks
+    )
 
 
 if __name__ == '__main__':
